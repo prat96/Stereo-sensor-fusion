40443d0cf4969b55840f942d41b037c44ebb80a7
prat96 <pratheekb96@gmail.com>
2019-11-07 21:23:33 +0200

    Add gaussian and laplacian pyramids

--- image_blending.py
+++ image_blending.py
@@ -26,5 +26,60 @@ def generating_kernel(a):
     return np.outer(w_1d, w_1d)
 
 
+'''reduce image by 1/2'''
+
+
+def ireduce(image):
+    out = None
+    kernel = generating_kernel(0.4)
+    outimage = scipy.signal.convolve2d(image, kernel, 'same')
+    out = outimage[::2, ::2]
+    return out
+
+
+'''expand image by factor of 2'''
+
+
+def iexpand(image):
+    out = None
+    kernel = generating_kernel(0.4)
+    outimage = np.zeros((image.shape[0] * 2, image.shape[1] * 2), dtype=np.float64)
+    outimage[::2, ::2] = image[:, :]
+    out = 4 * scipy.signal.convolve2d(outimage, kernel, 'same')
+    return out
+
+
+'''create a gaussain pyramid of a given image'''
+
+
+def gauss_pyramid(image, levels):
+    output = []
+    output.append(image)
+    tmp = image
+    for i in range(0, levels):
+        tmp = ireduce(tmp)
+        output.append(tmp)
+    return output
+
+
+'''build a laplacian pyramid'''
+
+
+def lapl_pyramid(gauss_pyr):
+    output = []
+    k = len(gauss_pyr)
+    for i in range(0, k - 1):
+        gu = gauss_pyr[i]
+        egu = iexpand(gauss_pyr[i + 1])
+        if egu.shape[0] > gu.shape[0]:
+            egu = np.delete(egu, (-1), axis=0)
+        if egu.shape[1] > gu.shape[1]:
+            egu = np.delete(egu, (-1), axis=1)
+        output.append(gu - egu)
+    output.append(gauss_pyr.pop())
+    return output
+
+
+
 if __name__ == '__main__':
     main()
